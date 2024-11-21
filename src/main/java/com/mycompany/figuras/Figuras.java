dcbb4bf80b9ad01e223ceae5363955f5f269ee65
iRibaldo <ribaldo.nantipbarre3a@gmail.com>
2024-11-21 14:18:30 -0500

    Preview

--- src/main/java/com/mycompany/figuras/Figuras.java
+++ src/main/java/com/mycompany/figuras/Figuras.java
@@ -12,6 +12,9 @@ public class Figuras {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
         System.out.println("Bienvenido");
+        
+        System.out.println(" hola Toni");
     }
 }
